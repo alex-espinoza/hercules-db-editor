ca7197f7460583a8083e7971f9b95eb40dcdaebb
Alex Espinoza <alex.espinoza@mullen.com>
2014-06-06 11:50:25 -0400

    Check uploaded db file for correct content_type

--- app/controllers/mobdb_controller.rb
+++ app/controllers/mobdb_controller.rb
@@ -1,5 +1,11 @@
 class MobdbController < ApplicationController
   def upload
+    database = params[:database]
+    
+    if !['text/plain', 'application/octet-stream'].include?(database.content_type)
+      return redirect_to mob_db_edit_path, alert: 'Please upload either a .conf or .txt mob db file.'
+    end
+
     redirect_to mob_db_edit_path
   end
 
