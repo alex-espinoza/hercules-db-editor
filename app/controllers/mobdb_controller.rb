abd9e895499357c096b7c2f840fb3f055496d2ba
Alex Espinoza <alex.espinoza@mullen.com>
2014-06-06 11:09:39 -0400

    Create mob database routes, controller, and views

---
+++ app/controllers/mobdb_controller.rb
@@ -0,0 +1,8 @@
+class MobdbController < ApplicationController
+  def upload
+    redirect_to mob_db_edit_path
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
+
+  post 'mob_db/upload' => 'mobdb#upload'
+  get 'mob_db/edit' => 'mobdb#edit'
 end
